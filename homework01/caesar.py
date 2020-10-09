c479f89ce256eb38062db1f97471773af1f2ed11
IrinaKramarenko <irinka.kramarenko288@gmail.com>
2020-10-10 01:16:05 +0300

    ecnrypt() and decrypt()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -16,6 +16,24 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    for i in plaintext:
+        if i.isalpha():
+            if i.isupper():
+                k = 63
+            else:
+                k = 95
+            c = ord(i) - k
+            if c + shift <= 26:
+                ciphertext += chr(c + shift +k)
+            else:
+                s = shift
+                while c <= 26 and s > 0:
+                    c += 1
+                    s -= 1
+                ciphertext += chr(1 + s + k)
+        else:
+            ciphertext += i 
+
     return ciphertext
 
 
@@ -34,6 +52,23 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if i.isalpha():
+            if i.isupper():
+                k = 63
+            else:
+                k = 95
+            c = ord(i) - k
+            if c - shift > 0:
+                plaintext += chr(c - shift + k)
+            else:
+                s = shift
+                while c > 0 and s > 0:
+                    c -= 1
+                    s -=1
+                plaintext += chr(26 - s + k)
+        else:
+            plaintext += i
     return plaintext
 
 
@@ -43,4 +78,9 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
     best_shift = 0
     # PUT YOUR CODE HERE
+    for d in dictionary:
+        if len(d) == len(ciphertext):
+            for shift in range(27):
+                if decryot_caesar(ciphertext, shift) == d:
+                    best_shift = shift
     return best_shift
