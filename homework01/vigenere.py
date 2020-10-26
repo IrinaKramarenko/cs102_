ddde1856d6ea316136e5b8f331fbc0337480de3a
Ира Крамаренко <70940678+IrinaKramarenko@users.noreply.github.com>
2020-10-26 21:01:19 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -21,20 +21,12 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
             t = 1
     for i in range(len(plaintext)):                          #перебор букв ввода и добавление зашифрованных элементов в вывод
         if plaintext[i].isalpha():
-            if plaintext[i].isupper():
-                shift_uni = 63
-            else:
-                shift_uni = 95
-            letter1 = ord(plaintext[i]) - shift_uni
             shift = ord(s[i]) - 65
-            if letter1 + shift <= 27:
-                ciphertext += chr(letter1 + shift + shift_uni)
+            if ord('Z') - shift < ord(plaintext[i]) <= ord('Z') or ord('z') - shift < ord(plaintext[i]) <= ord('z'):
+                ciphertext += chr(ord(plaintext[i]) - 26 + shift)
             else:
-                while letter1 <= 26 and shift > 0:
-                    letter1 += 1
-                    shift -= 1
-                ciphertext += chr(1 + shift + shift_uni)
-        else:                                               #добавление небуквенных элементов в вывод
+                ciphertext += chr(ord(plaintext[i]) + shift)
+        else:
             ciphertext += plaintext[i]
     return ciphertext
 
@@ -61,19 +53,11 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
             t = 1
     for i in range(len(ciphertext)):                          #перебор букв ввода и добавление расшифрованных элементов в вывод
         if ciphertext[i].isalpha():
-            if ciphertext[i].isupper():
-                shift_uni = 63
-            else:
-                shift_uni = 95
-            letter1 = ord(ciphertext[i]) - shift_uni
             shift = ord(s[i]) - 65
-            if letter1 - shift > 0:
-                plaintext += chr(letter1 - shift + shift_uni)
+            if ord('A') + shift > ord(ciphertext[i]) >= ord('A') or ord('a') + shift > ord(ciphertext[i]) >= ord('a'):
+                plaintext += chr(ord(ciphertext[i]) + 26 - shift)
             else:
-                while letter1 > 0 and shift > 0:
-                    letter1 -= 1
-                    shift -= 1
-                plaintext += chr(26 - shift + shift_uni)
-        else:                                         #добавление небуквенных элементов в вывод
+                plaintext += chr(ord(ciphertext[i]) - shift)
+        else:
             plaintext += ciphertext[i]
     return plaintext
