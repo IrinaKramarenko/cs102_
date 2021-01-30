c730e87eebf755ef7666fe1627420773fa83f3b7
Irina Kramarenko <irinka.kramarenko288@gmail.com>
2021-01-30 22:20:13 +0300

    mypy 3

--- homework04/pyvcs/refs.py
+++ homework04/pyvcs/refs.py
@@ -3,26 +3,27 @@ import typing as tp
 
 
 def update_ref(
-    gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value
+    gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value: str
 ) -> None:
     # PUT YOUR CODE HERE
     with open((gitdir / ref), "w") as file:
         file.write(new_value)
 
 
-def symbolic_ref(gitdir: pathlib.Path, name, ref) -> None:
+def symbolic_ref(gitdir: pathlib.Path, name: str, ref: str) -> None:
     # PUT YOUR CODE HERE
     with open(gitdir / name, "w") as file:
         file.write("ref: " + ref)
 
 
-def ref_resolve(gitdir: pathlib.Path, refname) -> str:
+def ref_resolve(gitdir: pathlib.Path, refname: str) -> str:
+    # PUT YOUR CODE HERE
     if refname == "HEAD" and not is_detached(gitdir):
         return resolve_head(gitdir)
     if (gitdir / refname).exists():
         with open(gitdir / refname) as file:
             return file.read().strip()
-    return None
+    return None  # type: ignore
 
 
 def resolve_head(gitdir: pathlib.Path) -> str:
